0d82c6993632f4c4a2ec5c6315c712a91ef783fa
dollaai <halodolla@gmail.com>
2026-07-12 22:21:38 +0700

    feat: implement cart functionality with controller, model, and migration files

--- database/migrations/2026_01_18_101646_create_products_table.php
+++ database/migrations/2026_01_18_101646_create_products_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,7 +15,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             
-            $table->uuid('uuid')->default(\DB::raw('(UUID())'));
+            $table->uuid('uuid')->default(DB::raw('(UUID())'));
+            $table->index('uuid');
             $table->unsignedBigInteger('seller_id');
             $table->string('slug');
             $table->string('name');
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\AddressController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\AuthenticationController;
-use App\Http\Controllers\ForgotPasswordController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/slider', [HomeController::class, 'getSlider']);
 Route::get('/category', [HomeController::class, 'getCategory']);
@@ -36,17 +37,30 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('province', [AddressController::class, 'getProvince']);
     Route::get('city', [AddressController::class, 'getCity']);
-});
+    Route::prefix('/')->group(function () {
+        Route::get('/', [HomeController::class, 'getIndex'])->name('home');
+    
+        // product
+        Route::prefix('product')->name('product.')->group(function () {
+            Route::get('/', [ProductController::class, 'getProduct'])->name('index');
+            Route::get('/{slug}', [ProductController::class, 'getProductDetail'])->name('show');
+            Route::get('/{slug}/reviews', [ProductController::class, 'getProductReview'])->name('reviews');
+        });
+    
+        Route::get('seller/{username}', [ProductController::class, 'getSellerDetail'])->name('seller');
+    
+        Route::prefix('cart')->name('cart.')->group(function () {
+            Route::get('/', [CartController::class, 'getCart'])->name('index');
+            Route::post('/', [CartController::class, 'addToCart'])->name('add');
+            Route::patch('/{uuid}', [CartController::class, 'updateCartItem'])->name('update');
+            Route::delete('/{uuid}', [CartController::class, 'removeCartItem'])->name('remove');
+            
+            
+            Route::get('/get-voucher', [CartController::class, 'getVoucher'])->name('get-voucher');
+            Route::post('/apply-voucher', [CartController::class, 'applyVoucher'])->name('apply-voucher');
+            Route::post('/remove-voucher', [CartController::class, 'removeVoucher'])->name('remove-voucher');
+        });
 
-Route::prefix('/')->group(function () {
-    Route::get('/', [HomeController::class, 'getIndex'])->name('home');
-
-    // product
-    Route::prefix('/product')->name('product.')->group(function () {
-        Route::get('/', [ProductController::class, 'getProduct'])->name('index');
-        Route::get('/{slug}', [ProductController::class, 'getProductDetail'])->name('show');
-        Route::get('/{slug}/reviews', [ProductController::class, 'getProductReview'])->name('reviews');
     });
-
-    Route::get('seller/{username}', [ProductController::class, 'getSellerDetail'])->name('seller');
 });
+
